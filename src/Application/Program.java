bb06104427d7b91ab0da30eecb5f753bd0a0c20d
Edumokfa <edumokfa@hotmail.com>
2021-06-21 16:29:00 -0300

    inserindo mais de uma linha

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -6,7 +6,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class Program {
@@ -19,7 +18,7 @@ public class Program {
         PreparedStatement st = null;
         try {
             conn = DB.getConnection();
-
+            /*
             st = conn.prepareStatement(
                     "INSERT INTO seller "
                     + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
@@ -30,6 +29,10 @@ public class Program {
             st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
             st.setDouble(4, 3000.0);
             st.setInt(5, 4);
+             */
+            st = conn.prepareStatement("insert into department (Name) values ('d1'),('d2')",
+                    Statement.RETURN_GENERATED_KEYS
+            );
 
             int rowsAffected = st.executeUpdate();
             if (rowsAffected > 0) {
@@ -42,8 +45,6 @@ public class Program {
                 System.out.println("Nenhuma linha alterada");
             }
         } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
             e.printStackTrace();
         } finally {
             DB.closeStatement(st);
