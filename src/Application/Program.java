49dd1e60fe106b186242787ba78c85c55052d80a
Edumokfa <edumokfa@hotmail.com>
2021-06-21 16:46:04 -0300

    Deletando dados

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,6 +1,7 @@
 package Application;
 
 import db.DB;
+import db.DbIntegrityException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -12,13 +13,17 @@ public class Program {
         PreparedStatement st = null;
         try {
             conn = DB.getConnection();
-            st = conn.prepareStatement("UPDATE seller SET BaseSalary = BaseSalary + ? WHERE (DepartmentId = ?);");
-            st.setDouble(1, 200.0);
-            st.setInt(2, 2);
+            st = conn.prepareStatement("DELETE FROM department "
+                    + "WHERE "
+                    + "Id = ?");
+            st.setInt(1, 2);
             int rowsAffected = st.executeUpdate();
             System.out.println("Pronto, Linhas afetadas: " + rowsAffected);
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new DbIntegrityException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
         }
     }
 
